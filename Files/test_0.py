278cc54736c4d46b0de69d0a8f199c9cc105eab3
sartaj <sartajcorp@gmail.com>
2016-06-23 17:09:03 +0200

    Code Modularized, Different Queries Tested

--- Files/test_0.py
+++ Files/test_0.py
@@ -2,51 +2,63 @@
 import json, random, string, pymongo
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.code
+def init():
+    client = MongoClient()
+    db = client.code
+    doc = loadJSON()
+    # createIndex(db)
+    # deleteCollection(db)
+    # loadFiles(db, doc)
+    query(db)
 
-# print db.production.create_index([("LFNArray", pymongo.ASCENDING)])
+def createIndex(db):
+    print db.production.create_index([("LFNArray", pymongo.ASCENDING)])
 
-with open('fwjr_prod.json') as data_file:
-    doc = json.load(data_file)
+def loadJSON():
+    with open('fwjr_prod.json') as data_file:
+        doc = json.load(data_file)
+    return doc
 
-cursor = db.production.insert(doc)
+def loadFiles(db, doc):
+    cursor = db.production.insert(doc)
 
-for idx in range(3):
-    newdoc = dict(doc)
-    del newdoc["_id"]
-    
-    newdoc["wmaid"] = idx
+    for idx in range(3):
+        newdoc = dict(doc)
+        del newdoc["_id"]
 
-    newdoc["steps"][0]["performance"]["storage"]["writeTotalMB"] = round(random.uniform(200, 400), 2)
-    newdoc["steps"][0]["performance"]["storage"]["readPercentageOps"] = random.uniform(1, 2)
-    newdoc["steps"][0]["performance"]["storage"]["readMBSec"] = random.random()
+        newdoc["wmaid"] = idx
 
-    rand1 = "".join([random.choice(string.ascii_letters + string.digits) for n in xrange(5)])
-    rand2 = "".join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+        newdoc["steps"][0]["performance"]["storage"]["writeTotalMB"] = round(random.uniform(200, 400), 2)
+        newdoc["steps"][0]["performance"]["storage"]["readPercentageOps"] = random.uniform(1, 2)
+        newdoc["steps"][0]["performance"]["storage"]["readMBSec"] = random.random()
 
-    newdoc["steps"][0]["output"][0]["inputDataset"] = "/Cosmics/Run-" + rand1
-    newdoc["steps"][0]["output"][0]["branch_hash"] = rand2
+        rand1 = "".join([random.choice(string.digits) for n in xrange(5)])
+        rand2 = "".join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
 
-    for i in range(len(newdoc["LFNArray"])):
-        newdoc["LFNArray"][i] = "/store/mc/Run"+str(idx)+"/file"+str(i)+".root"
+        newdoc["steps"][0]["output"][0]["inputDataset"] = "/Cosmics/Run-" + rand1
+        newdoc["steps"][0]["output"][0]["branch_hash"] = rand2
 
-    for i in range(len(newdoc["PFNArray"])):
-        newdoc["PFNArray"][i] = "root://test.ch/Run"+str(idx)+"/file"+str(i)+".root"
+        for i in range(len(newdoc["LFNArray"])):
+            newdoc["LFNArray"][i] = "/store/mc/Run"+str(idx)+"/file"+str(i)+".root"
 
-    db.production.insert(newdoc)
+        for i in range(len(newdoc["PFNArray"])):
+            newdoc["PFNArray"][i] = "root://test.ch/Run"+str(idx)+"/file"+str(i)+".root"
 
-print("Number of docs", db.production.count())
+        db.production.insert(newdoc)
 
-cursor = db.production.drop()
+    print("Number of docs", db.production.count())
 
-''' Query Language '''
+def deleteCollection(db):
+    cursor = db.production.drop()
 
-cursor = db.production.find({"wmaid":0})
-cursor = db.production.find({ 'PFNArray' : { '$regex' : r'^root://test.ch/R' } }) # All regex that starts with root:// ....
-cursor = db.production.find({ 'LFNArray' : { '$regex' : r'^/store/mc/Run' } }) # All regex that starts with /store/mc/Run ....
-cursor = db.production.find({ 'steps.output.inputDataset' : { '$regex' : r'^/Cosmics/Run-' } }) # All regex that starts with /store/mc/Run ....
+def query(db):
+    cursor = db.production.find({"wmaid":0})
+    cursor = db.production.find({ 'PFNArray' : { '$regex' : r'^root://test.ch/R' } }) # All regex that starts with root:// ....
+    cursor = db.production.find({ 'LFNArray' : { '$regex' : r'^/store/mc/Run' } }) # All regex that starts with /store/mc/Run ....
+    cursor = db.production.find({ 'steps.output.inputDataset' : { '$regex' : r'^/Cosmics/Run-' } }) # All regex that starts with /store/mc/Run ....
 
-for document in cursor:
-    print document
+    for document in cursor:
+        print document
 
+if __name__ == "__main__":
+    init()

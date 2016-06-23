c53fba544d2ed6bc069f6ca72910922152c84024
sartaj <sartajcorp@gmail.com>
2016-06-23 09:57:55 +0200

    Different Queries added

--- Files/test_0.py
+++ Files/test_0.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env Python
-import json, random, string
+import json, random, string, pymongo
 from pymongo import MongoClient
 
 client = MongoClient()
 db = client.code
+
+# print db.production.create_index([("LFNArray", pymongo.ASCENDING)])
 
 with open('fwjr_prod.json') as data_file:
     doc = json.load(data_file)
@@ -13,6 +15,7 @@ cursor = db.production.insert(doc)
 for idx in range(3):
     newdoc = dict(doc)
     del newdoc["_id"]
+    
     newdoc["wmaid"] = idx
 
     newdoc["steps"][0]["performance"]["storage"]["writeTotalMB"] = round(random.uniform(200, 400), 2)
@@ -35,4 +38,15 @@ for idx in range(3):
 
 print("Number of docs", db.production.count())
 
-# cursor = db.production.drop()
+cursor = db.production.drop()
+
+''' Query Language '''
+
+cursor = db.production.find({"wmaid":0})
+cursor = db.production.find({ 'PFNArray' : { '$regex' : r'^root://test.ch/R' } }) # All regex that starts with root:// ....
+cursor = db.production.find({ 'LFNArray' : { '$regex' : r'^/store/mc/Run' } }) # All regex that starts with /store/mc/Run ....
+cursor = db.production.find({ 'steps.output.inputDataset' : { '$regex' : r'^/Cosmics/Run-' } }) # All regex that starts with /store/mc/Run ....
+
+for document in cursor:
+    print document
+

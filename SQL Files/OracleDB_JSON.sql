6f0697b4436b96dfeeb2c9d652ee5f17bfd2ad56
sartaj <sartajcorp@gmail.com>
2016-07-08 17:41:50 +0200

    SQL File Updated

--- SQL Files/OracleDB_JSON.sql
+++ SQL Files/OracleDB_JSON.sql
@@ -1,48 +1,15 @@
 # Create Table 
 
-CREATE TABLE j_purchaseorder
+CREATE TABLE CMSDocument
    (id          RAW (16) NOT NULL,
     date_loaded TIMESTAMP (6) WITH TIME ZONE,
-    po_document CLOB
+    doc CLOB
     CONSTRAINT ensure_json CHECK (po_document IS JSON));
-
-# Insert Statement but valid for string literals upto 4000 characters
-
-INSERT INTO j_purchaseorder
-  VALUES (
-    SYS_GUID(),
-    SYSTIMESTAMP,
-    '{"PONumber"             : 1600,
-      "Reference"            : "ABULL-20140421",
-      "Requestor"            : "Alexis Bull",
-      "User"                 : "ABULL",
-      "CostCenter"           : "A50",
-      "ShippingInstructions" : {"name"   : "Alexis Bull",
-                                "Address": {"street"  : "200 Sporting Green",
-                                            "city"    : "South San Francisco",
-                                            "state"   : "CA",
-                                            "zipCode" : 99236,
-                                            "country" : "United States of America"},
-                                "Phone" : [{"type" : "Office", "number" : "909-555-7307"},
-                                           {"type" : "Mobile", "number" : "415-555-1234"}]},
-      "Special Instructions" : null,
-      "AllowPartialShipment" : true,
-      "LineItems"            : [{"ItemNumber" : 1,
-                                 "Part"       : {"Description" : "One Magic Christmas",
-                                                 "UnitPrice"   : 19.95,
-                                                 "UPCCode"     : 13131092899},
-                                 "Quantity"   : 9.0},
-                                {"ItemNumber" : 2,
-                                 "Part"       : {"Description" : "Lethal Weapon",
-                                                 "UnitPrice"   : 19.95,
-                                                 "UPCCode"     : 85391628927},
-                                 "Quantity"   : 5.0}]}'
-          );
 
 # Insert using Declaring a variable
 
 DECLARE 
-  DOC2 varchar2(32767);
+  DOC2 CLOB;
 BEGIN
   DOC2 := '{
     "LFNArray": [
